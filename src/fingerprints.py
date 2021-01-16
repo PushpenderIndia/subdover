8165d8fae7127aad4df28f0a5e64eceb4adb5232
Pushpender <56433698+PushpenderIndia@users.noreply.github.com>
2021-01-16 09:43:54 +0530

    Added 3 New Fingerprints

--- src/fingerprints.py
+++ src/fingerprints.py
@@ -83,38 +83,38 @@ Available Fingerprints
 
 
 f1 = [
-	"Acquia", #Service
-	"Vulnerable", #Status
+        "Acquia", #Service
+        "Vulnerable", #Status
         ["acquia-test.co"], #CNAME
-	"The site you are looking for could not be found."  #Fingerprint
+        "The site you are looking for could not be found."  #Fingerprint
 	]
 
 f2 = [
-	"ActiveCampaign",
-	"Vulnerable",
+        "ActiveCampaign",
+        "Vulnerable",
         ["activehosted.com"],  
-	"alt=\"LIGHTTPD - fly light.\""
+        "alt=\"LIGHTTPD - fly light.\""
 	]
     
 f3 = [
-	"AfterShip",
-	"Vulnerable",
+        "AfterShip",
+        "Vulnerable",
         ["aftership.com"],  
-	"Oops.</h2><p class=\"text-muted text-tight\">The page you're looking for doesn't exist."
+        "Oops.</h2><p class=\"text-muted text-tight\">The page you're looking for doesn't exist."
 	]
     
 f4 = [
-	"AgileCRM",
-	"Vulnerable",
-	["cname.agilecrm.com", "agilecrm.com"],
-	"Sorry, this page is no longer available."
+        "AgileCRM",
+        "Vulnerable",
+        ["cname.agilecrm.com", "agilecrm.com"],
+        "Sorry, this page is no longer available."
 	]    
 
 f5 = [
-	"Aha",
-	"Vulnerable",
+        "Aha",
+        "Vulnerable",
         ["ideas.aha.io"],  
-	"There is no portal here ... sending you back to Aha!"
+        "There is no portal here ... sending you back to Aha!"
 	]
     
 f6 = [
@@ -135,38 +135,38 @@ f7 = [
 	]     
 
 f8 = [
-	"Apigee",
-	"Vulnerable",
+        "Apigee",
+        "Vulnerable",
         ["-portal.apigee.net"],  
-	""  #NoResponse_HenceNoFingerprint
+        ""  #NoResponse_HenceNoFingerprint
 	]   
 
 f9 = [
-	"AWS/S3",
-	"Vulnerable",
+        "AWS/S3",
+        "Vulnerable",
         ["amazonaws"],  
-	"The specified bucket does not exist"
+        "The specified bucket does not exist"
 	]      
     
 f10 = [
-	"Bigcartel",
-	"Vulnerable",
+        "Bigcartel",
+        "Vulnerable",
         ["bigcartel.com"],
-	"<h1>Oops! We could&#8217;t find that page.</h1>"
+        "<h1>Oops! We could&#8217;t find that page.</h1>"
     ]
 
 f11 = [
-	"Bitbucket",
-	"Vulnerable",
+        "Bitbucket",
+        "Vulnerable",
         ["bitbucket.io"],
-	"Repository not found" 
+        "Repository not found" 
 	] 
 
 f12 = [
-	"Brightcove",
-	"Vulnerable",
+        "Brightcove",
+        "Vulnerable",
         ["bcvp0rtal.com", "brightcovegallery.com", "gallery.video"],
-	"<p class=\"bc-gallery-error-code\">Error Code: 404</p>",
+        "<p class=\"bc-gallery-error-code\">Error Code: 404</p>",
 	]      
 
 f13 = [
@@ -178,38 +178,38 @@ f13 = [
 	] 
 
 f14 = [
-	"CampaignMonitor",
-	"Vulnerable",
+        "CampaignMonitor",
+        "Vulnerable",
         ["createsend.com", "name.createsend.com"], 
-	"Double check the URL or <a href=\"mailto:help@createsend.com"
+        "Double check the URL or <a href=\"mailto:help@createsend.com"
 	]
     
 f15 = [
-	"Cargo",
-	"Vulnerable",
+        "Cargo",
+        "Vulnerable",
         ["cargocollective.com"], 
-	"If you're moving your domain away from Cargo you must make this configuration through your registrar's DNS control panel."
+        "If you're moving your domain away from Cargo you must make this configuration through your registrar's DNS control panel."
 	]
 
 f16 = [
-	"CargoCollective",
-	"Vulnerable",
+        "CargoCollective",
+        "Vulnerable",
         ["subdomain.cargocollective.com"], 
-	"404 Not Found",
+        "404 Not Found"
 	]
 
 f17 = [
-	"Cloudfront",
-	"Edge case ",
+        "Cloudfront",
+        "Edge case ",
         ["cloudfront.net"],
-	"Bad Request: ERROR: The request could not be satisfied",
+        "Bad Request: ERROR: The request could not be satisfied"
 	]    
 
 f18 = [
-	"Desk",
-	"Not vulnerable",
+        "Desk",
+        "Not vulnerable",
         ["desk.com"],
-	"Please try again or try Desk.com free for 14 days.",
+        "Please try again or try Desk.com free for 14 days."
 	]
     
 f19 = [
@@ -220,59 +220,59 @@ f19 = [
     ]     
 
 f20 = [
-	"Fastly",
-	"Edge case ",
+        "Fastly",
+        "Edge case ",
         ["fastly.net"],
-	"Fastly error: unknown domain:",
+        "Fastly error: unknown domain:"
 	]
 
 f21 = [
-	"Feedpress",
-	"Vulnerable",
+        "Feedpress",
+        "Vulnerable",
         ["redirect.feedpress.me"],
-	"The feed has not been found."
+        "The feed has not been found."
 	]
     
 f22 = [
-	"Freshdesk",
-	"Vulnerable",  #NotSure
+        "Freshdesk",
+        "Vulnerable",  #NotSure
         ["freshdesk.com"],
-	"May be this is still fresh!"
+        "May be this is still fresh!"
 	] 
 
 f23 = [
         "Frontify",
         "Vulnerable",
         ["frontify.com"],
-        "404 - Page not found. Oops... look like you got lost."
+        "404 - Page Not Found</h1>"
 	]     
 
 f24 = [
-	"GetResponse",
-	"Vulnerable",
+        "GetResponse",
+        "Vulnerable",
         [".gr8.com"],
-	"With GetResponse Landing Pages, lead generation has never been easier"
+        "With GetResponse Landing Pages, lead generation has never been easier"
 	]
 
 f25 = [
-	"Ghost",
-	"Vulnerable",
+        "Ghost",
+        "Vulnerable",
         ["ghost.io"],
-	"The thing you were looking for is no longer here, or never was"
+        "The thing you were looking for is no longer here, or never was"
 	]
 
 f26 = [
-	"Github",
-	"Vulnerable",
+        "Github",
+        "Vulnerable",
         ["github.io"],
-	"There isn't a GitHub Pages site here."
+        "There isn't a GitHub Pages site here."
 	]
 
 f27 = [
-	"Help Juice",
-	"Vulnerable",
+        "Help Juice",
+        "Vulnerable",
         ["helpjuice.com"],
-	"We could not find what you're looking for"
+        "We could not find what you're looking for"
 	]
 
 f28 = [
@@ -284,52 +284,52 @@ f28 = [
     ] 
 
 f29 = [
-	"Help Scout",
-	"Vulnerable",
+        "Help Scout",
+        "Vulnerable",
         ["helpscoutdocs.com"],
-	"No settings were found for this company"
+        "No settings were found for this company"
 	]
 
 f30 = [
-	"Heroku",
-	"Edge case ",
+        "Heroku",
+        "Edge case ",
         ["herokuapp"],
-	"No such app"
+        "No such app"
 	]
     
 f31 = [
         "Hubspot",
         "Vulnerable",   ##Not Sure
         ["sites.hubspot.net"],
-        "Domain Not found",
+        "Domain Not found"
 	]      
 
 f32 = [
-	"Instapage",
-	"Vulnerable",
+        "Instapage",
+        "Vulnerable",
         ["pageserve.co", "secure.pageserve.co", "https://instapage.com/"],
-	"You've Discovered A Missing Link. Our Apologies!"
+        "You've Discovered A Missing Link. Our Apologies!"
 	]
     
 f33 = [
-	"InterCom",
-	"Vulnerable",
+        "InterCom",
+        "Vulnerable",
         ["custom.intercom.help"],
-	"<h1 class=\"headline\"Uh oh. That page doesn't exist.</h1>"
+        "<h1 class=\"headline\"Uh oh. That page doesn't exist.</h1>"
 	]    
 
 f34 = [
-	"JetBrains",
-	"Vulnerable",
+        "JetBrains",
+        "Vulnerable",
         ["myjetbrains.com"],
-	"is not a registered InCloud YouTrack"
+        "is not a registered InCloud YouTrack"
 	]
 
 f35 = [
-	"Kajabi",
-	"Vulnerable",
+        "Kajabi",
+        "Vulnerable",
         ["endpoint.mykajabi.com"],
-	"<h1>The page you were looking for doesn't exist.</h1>"
+        "<h1>The page you were looking for doesn't exist.</h1>"
 	]
 
 f36 = [
@@ -365,17 +365,17 @@ f38 = [
 	] 
     
 f39 = [
-	"Mashery",
-	"Edge Case ",
+        "Mashery",
+        "Edge Case ",
         ["mashery.com"],
-	"Unrecognized domain"
+        "Unrecognized domain"
 	]    
 
 f40 = [
-	"MicrosoftAzure",
-	"Vulnerable",  
+        "MicrosoftAzure",
+        "Vulnerable",  
         ["cloudapp.net", "cloudapp.azure.com", "azurewebsites.net", "blob.core.windows.net", "cloudapp.azure.com", "azure-api.net", "azurehdinsight.net", "azureedge.net", "azurecontainer.io", "database.windows.net", "azuredatalakestore.net", "search.windows.net", "azurecr.io", "redis.cache.windows.net", "azurehdinsight.net", "servicebus.windows.net", "visualstudio.com"],
-	"404 Web Site not found"     
+        "404 Web Site not found"     
 	]
     
 f41 = [
@@ -386,31 +386,31 @@ f41 = [
 	]           
 
 f42 = [
-	"Pantheon",
-	"Vulnerable",
+        "Pantheon",
+        "Vulnerable",
         ["pantheonsite.io"],
-	"The gods are wise, but do not know of the site which you seek."
+        "The gods are wise, but do not know of the site which you seek."
 	]
     
 f43 = [
-	"Pingdom",
-	"Vulnerable",
+        "Pingdom",
+        "Vulnerable",
         ["stats.pingdom.com"],
-	"This public report page has not been activated by the user"
+        "This public report page has not been activated by the user"
 	]
 
 f44 = [
-	"Proposify",
-	"Vulnerable",
+        "Proposify",
+        "Vulnerable",
         ["proposify.biz"],
-	"If you need immediate assistance, please contact <a href=\"mailto:support@proposify.biz"
+        "If you need immediate assistance, please contact <a href=\"mailto:support@proposify.biz"
 	]
 
 f45 = [
-	"Readme.io",
-	"Vulnerable",
+        "Readme.io",
+        "Vulnerable",
         ["readme.io"],
-	"Project doesnt exist... yet!"
+        "Project doesnt exist... yet!"
 	] 
 
 f46 = [
@@ -421,101 +421,101 @@ f46 = [
 	]    
 
 f47 = [
-	"Shopify",
-	"Edge Case ",
+        "Shopify",
+        "Edge Case ",
         ["myshopify.com"],
-	"Sorry, this shop is currently unavailable"
+        "Sorry, this shop is currently unavailable"
 	]
 
 f48 = [
-	"SimpleBooklet",
-	"Vulnerable",
+        "SimpleBooklet",
+        "Vulnerable",
         ["simplebooklet.com"],
-	"We can't find this <a href=\"https://simplebooklet.com"
+        "We can't find this <a href=\"https://simplebooklet.com"
 	]
     
 f49 = [
-	"Smartling",
-	"Vulnerable",
+        "Smartling",
+        "Vulnerable",
         ["smartling.com"],
-	"Domain is not configured"
+        "Domain is not configured"
 	]
 
 f50 = [
-	"Smugmug",
-	"Vulnerable",
+        "Smugmug",
+        "Vulnerable",
         ["domains.smugmug.com"],
-	""  #NotAvailable
+        ""  #NotAvailable
 	]      
 
 f51 = [
-	"StatusPage",
-	"Vulnerable",
+        "StatusPage",
+        "Vulnerable",
         ["statuspage.io"],
-	"You are being <a href=\"https://www.statuspage.io\">redirected"
+        "You are being <a href=\"https://www.statuspage.io\">redirected"
 	]
 
 f52 = [
-	"Strikingly",
-	"Vulnerable",
+        "Strikingly",
+        "Vulnerable",
         [".s.strikinglydns.com"],
-	"But if you're looking to build your own website,"
+        "But if you're looking to build your own website,"
 	]
 
 f53 = [
-	"Surge.sh",
-	"Vulnerable",
+        "Surge.sh",
+        "Vulnerable",
         ["surge.sh"],
-	"project not found"
+        "project not found"
 	]    
 
 f54 = [
-	"Surveygizmo",
-	"Vulnerable",
+        "Surveygizmo",
+        "Vulnerable",
         ["privatedomain.sgizmo.com", "privatedomain.surveygizmo.eu", "privatedomain.sgizmoca.com"],
-	"data-html-name"
+        "data-html-name"
 	] 
 
 f55 = [
-	"Tave",
-	"Vulnerable",
+        "Tave",
+        "Vulnerable",
         ["clientaccess.tave.com"],
-	"<h1>Error 404: Page Not Found</h1>"
+        "<h1>Error 404: Page Not Found</h1>"
 	]
     
 f56 = [
-	"Teamwork",
-	"Vulnerable",
+        "Teamwork",
+        "Vulnerable",
         ["teamwork.com"],
-	"Oops - We didn't find your site."
+        "Oops - We didn't find your site."
 	]
 
 f57 = [
-	"Thinkific",
-	"Vulnerable",
+        "Thinkific",
+        "Vulnerable",
         ["thinkific.com"],
-	"You may have mistyped the address or the page may have moved."
+        "You may have mistyped the address or the page may have moved."
 	]
 
 f58 = [
-	"Tictail",
-	"Vulnerable",
+        "Tictail",
+        "Vulnerable",
         ["domains.tictail.com"],
-	"to target URL: <a href=\"https://tictail.com", "Start selling on Tictail."
+        "to target URL: <a href=\"https://tictail.com", "Start selling on Tictail."
 	]
 
 f59 = [
-	"Tilda",
-	"Edge Case ",
+        "Tilda",
+        "Edge Case ",
         ["tilda.ws"],
-	"Please renew your subscription"
+        "Please renew your subscription"
 	]
 
 f60 = [
-	"Tumblr",
-	"Vulnerable",
+        "Tumblr",
+        "Vulnerable",
         ["domains.tumblr.com"],
-	"Whatever you were looking for doesn't currently exist at this address"
+        "Whatever you were looking for doesn't currently exist at this address"
 	]    
 
 f61 = [
@@ -526,45 +526,45 @@ f61 = [
 	] 
 
 f62 = [
-	"Unbounce",
-	"Edge Case ",
+        "Unbounce",
+        "Edge Case ",
         ["unbouncepages.com"],
-	"The requested URL was not found on this server"
+        "The requested URL was not found on this server"
 	]
 
 f63 = [
-	"UptimeRobot",
-	"Vulnerable",
+        "UptimeRobot",
+        "Vulnerable",
         ["stats.uptimerobot.com"],
-	"This public status page <b>does not seem to exist</b>."
+        "This public status page <b>does not seem to exist</b>."
 	]
 
 f64 = [
-	"UserVoice",
-	"Vulnerable",
+        "UserVoice",
+        "Vulnerable",
         ["uservoice.com"],
-	"This UserVoice subdomain is currently available"
+        "This UserVoice subdomain is currently available"
 	]
 
 f65 = [
-	"Vend",
-	"Vulnerable",
+        "Vend",
+        "Vulnerable",
         ["vendecommerce.com"],
-	"Looks like you've traveled too far into cyberspace"
+        "Looks like you've traveled too far into cyberspace"
 	]
     
 f66 = [
-	"WebFlow",
-	"Vulnerable",
+        "WebFlow",
+        "Vulnerable",
         ["proxy.webflow.com", "proxy-ssl.webflow.com"],
-	"<p class=\"description\">The page you are looking for doesn't exist or has been moved.</p>"
+        "<p class=\"description\">The page you are looking for doesn't exist or has been moved.</p>"
 	]
 
 f67 = [
-	"WishPond",
-	"Vulnerable",
+        "WishPond",
+        "Vulnerable",
         ["wishpond.com"],
-	"https://www.wishpond.com/404?campaign=true"
+        "https://www.wishpond.com/404?campaign=true"
 	]
     
 f68 = [
@@ -577,20 +577,40 @@ f68 = [
     
 
 f69 = [
-	"Wordpress",
-	"Vulnerable",
+        "Wordpress",
+        "Vulnerable",
         ["wordpress.com"],
-	"Do you want to register "
+        "Do you want to register "
 	]    
 
 f70 = [
-	"Zendesk",
-	"Not Vulnerable",
+        "Zendesk",
+        "Not Vulnerable",
         ["zendesk.com"],
-	"Help Center Closed"
+        "Help Center Closed"
 	]
-      
-
+ 
+f71 =  [
+        "Appery.io",
+        "Vulnerable",
+        [""],  #404 Cname
+        "<p>This page will be updated automatically when your app is published.</p>",
+        # Pointing to 107.20.248.61
+    ]
+ 
+f72 = [
+        "Vercel.com",
+        "Vulnerable",
+        [""], #404 Cname
+        "The deployment could not be found on Vercel."
+    ]
+   
+f73 = [
+        "Datocms.com",
+        "Vulnerable",
+        [""], #404 Cname
+        "<!doctype html><html><head><meta charset=\"utf-8\"><title>Loading...</title>",
+    ]    
 """
 Kinsta
 Edge Case
@@ -601,6 +621,11 @@ Edge Case
 # Content-Length=[33604]
 # Server = kinsta-nginx
 
-"""     
+"""    
 
-fingerprints_list = [f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f22,f23,f24,f25,f26,f27,f28,f29,f30,f31,f32,f33,f34,f35,f36,f37,f38,f39,f40,f41,f42,f43,f44,f45,f46,f47,f48,f49,f50,f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61,f62,f63,f64,f65,f66,f67,f68,f69,f70]
+
+
+ 
+
+
+fingerprints_list = [f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f22,f23,f24,f25,f26,f27,f28,f29,f30,f31,f32,f33,f34,f35,f36,f37,f38,f39,f40,f41,f42,f43,f44,f45,f46,f47,f48,f49,f50,f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61,f62,f63,f64,f65,f66,f67,f68,f69,f70,f71,f72,f73]
