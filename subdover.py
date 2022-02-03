5e2d2fce97256fdaa7d0f24d4aef379ef15370d6
Pushpender Singh <singhpushpender250@gmail.com>
2022-02-03 18:03:39 +0530

    Version 1.5 [Added a flag to skip HTTPx (HTTP Probing)]

--- subdover.py
+++ subdover.py
@@ -10,6 +10,10 @@ import numpy as np
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
@@ -25,14 +29,15 @@ findomain_path = subdover_dir + "externals/findomain.exe"
 httpx_path = subdover_dir + "externals/httpx.exe"
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description=f'{RED}SubDover v1.4')
-    parser._optionals.title = f"{GREEN}Optional Arguments{YELLOW}"
+    parser = argparse.ArgumentParser(description=f'{Fore.RED}SubDover v1.5')
+    parser._optionals.title = f"{Fore.GREEN}Optional Arguments{Fore.YELLOW}"
     parser.add_argument("-t", "--thread", dest="thread", help="Number of Threads to Used. Default=10", default=10)
     parser.add_argument("-o", "--output", dest="output", help="Save Result in TXT file")
-    parser.add_argument("--update", dest="check_and_update", help="Check & Update Subdover", action='store_true')
+    parser.add_argument("-u", "--update", dest="check_and_update", help="Check & Update Subdover", action='store_true')
+    parser.add_argument("-skip", "--skip-httpx", dest="skip_httpx_probing", help=F"Skip HTTP/HTTPS Protocal Resolution ({Fore.WHITE}HTTP Probing{Fore.YELLOW}) [{Fore.RED}NOTE{Fore.YELLOW}]: You must manually use httpx/httprobe on your subdomain list & then provide that final subdomains list using {Fore.GREEN}--list {Fore.YELLOW}or {Fore.GREEN}-l{Fore.YELLOW} flag", action='store_true')
     parser.add_argument("-s", "--fingerprints", dest="show_fingerprint", help="Show Available Fingerprints & Exit", action='store_true')   
     
-    required_arguments = parser.add_argument_group(f'{RED}Required Arguments{GREEN}')
+    required_arguments = parser.add_argument_group(f'{Fore.RED}Required Arguments{Fore.GREEN}')
     required_arguments.add_argument("-d", "--domain", dest="domain", help="Target Wildcard Domain [For AutoSubdomainEnumeration], ex:- google.com")
     required_arguments.add_argument("-l", "--list", dest="subdomain_list", help="Target Subdomain List, ex:- google_subdomain.txt")
     return parser.parse_args()
@@ -40,16 +45,16 @@ def get_arguments():
 def check_dependencies(commandToCheck):
     a = subprocess.run(str(commandToCheck), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     if a.returncode == 0:
-        print(f"{GREEN}[+] {WHITE}{commandToCheck.split()[0]} {GREEN}is Installed on your system.{WHITE}")
+        print(f"{Fore.GREEN}[+] {Fore.WHITE}{commandToCheck.split()[0]} {Fore.GREEN}is Installed on your system.{Fore.WHITE}")
     else:
-        print(f"{RED}[!] {WHITE}{commandToCheck.split()[0]}{RED} is not Installed on your system.{WHITE}")
+        print(f"{Fore.RED}[!] {Fore.WHITE}{commandToCheck.split()[0]}{Fore.RED} is not Installed on your system.{Fore.WHITE}")
         sys.exit()
 
 def check_and_update():            
     try:
         ongoing_version = requests.get("https://raw.githubusercontent.com/PushpenderIndia/subdover/master/version.txt")
     except Exception:
-        print(f"{WHITE}[{RED}ERR{WHITE}] No Internet Connection")
+        print(f"{Fore.WHITE}[{Fore.RED}ERR{Fore.WHITE}] No Internet Connection")
         sys.exit()
         
     ongoing_version = ongoing_version.text.strip()
@@ -57,15 +62,15 @@ def check_and_update():
     with open(subdover_dir+"version.txt", "r") as f:
         currentVersion = f.read()
         if currentVersion != ongoing_version:
-            print(f"{YELLOW}[*] Installing Subdover v{ongoing_version}")
+            print(f"{Fore.YELLOW}[*] Installing Subdover v{ongoing_version}")
             subprocess.run("git pull origin master", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-            print(f"{GREEN}[+] Updated to latest version: v{ongoing_version}..")
+            print(f"{Fore.GREEN}[+] Updated to latest version: v{ongoing_version}..")
             
             with open(subdover_dir+"version.txt", "w") as f:
                 f.write(ongoing_version)
             sys.exit()
         else:
-            print(f"{GREEN}[+] Subdover is already Up-to-Date: v{ongoing_version}..")
+            print(f"{Fore.GREEN}[+] Subdover is already Up-to-Date: v{ongoing_version}..")
             sys.exit()
 
 def readTargetFromFile(filepath):
@@ -152,7 +157,7 @@ def testTarget(url):
         error = fingerprint[3]
         
         if targetResponse == "ConnectionError_SubDover":
-            print(f"{RED}[!] ConnectionError : {WHITE}{url}")
+            print(f"{Fore.RED}[!] ConnectionError : {Fore.WHITE}{url}")
             not_success = False
             break
         
@@ -164,7 +169,7 @@ def testTarget(url):
                 service_cname_list = fingerprint[2]
                 confirm, enumeratedCNAME = confirm_vulnerable(url, service_cname_list)
                 if confirm == True:            
-                    print(f"{GREEN}[+] {fingerprint[1]} ===> : {WHITE}[{RED}Service{WHITE}: {fingerprint[0]}] {WHITE}[{RED}CNAME{WHITE}: {enumeratedCNAME}] : {GREEN}{url}{WHITE}")
+                    print(f"{Fore.GREEN}[+] {fingerprint[1]} ===> : {Fore.WHITE}[{Fore.RED}Service{Fore.WHITE}: {fingerprint[0]}] {Fore.WHITE}[{Fore.RED}CNAME{Fore.WHITE}: {enumeratedCNAME}] : {Fore.GREEN}{url}{Fore.WHITE}")
                     not_success = False
                     if arguments.output:
                         with open(arguments.output, "a") as f:
@@ -175,7 +180,7 @@ def testTarget(url):
                     #CargoCollective & Akamai fingerprints can leads to False +ve 
                     #If script is unable to confirm detection using CNAME, then we will ignore that detection
                     
-                    print(f"{GREEN}[+] {fingerprint[1]} ===> : {WHITE}[{RED}Service{WHITE}: {fingerprint[0]}] {WHITE}[{RED}CNAME{WHITE}: 404, UnableToVerify-CouldBeFalsePositive] : {GREEN}{url}{WHITE}")
+                    print(f"{Fore.GREEN}[+] {fingerprint[1]} ===> : {Fore.WHITE}[{Fore.RED}Service{Fore.WHITE}: {fingerprint[0]}] {Fore.WHITE}[{Fore.RED}CNAME{Fore.WHITE}: 404, UnableToVerify-CouldBeFalsePositive] : {Fore.GREEN}{url}{Fore.WHITE}")
                     not_success = False
                     if arguments.output:
                         with open(arguments.output, "a") as f:
@@ -183,14 +188,14 @@ def testTarget(url):
                     break                    
          
     if not_success:
-        print(f"{WHITE}[-] Not Vulnerable  : {GREEN}{url}{WHITE}")        
+        print(f"{Fore.WHITE}[-] Not Vulnerable  : {Fore.GREEN}{url}{Fore.WHITE}")        
 
 def start_scanning(subdomain_list):
     for subdomain in subdomain_list:
         testTarget(subdomain)     
 
 if __name__ == '__main__':
-    print(f"\t\t{YELLOW}Author: {GREEN}Pushpender Singh  | {YELLOW}GitHub: {GREEN}PushpenderIndia\n{WHITE}")
+    print(f"\t\t{Fore.YELLOW}Author: {Fore.GREEN}Pushpender Singh  | {Fore.YELLOW}GitHub: {Fore.GREEN}PushpenderIndia\n{Fore.WHITE}")
     arguments = get_arguments() 
     
     if AttackerSystem == "Linux":
@@ -219,14 +224,14 @@ if __name__ == '__main__':
                 git_path = subprocess.check_output("where git", shell=True)
                 check_and_update()
             except Exception:
-                print(f"{WHITE}[{RED}ERR{WHITE}] GIT is NOT Installed on Your System! You can't use AutoUpdater .") 
+                print(f"{Fore.WHITE}[{Fore.RED}ERR{Fore.WHITE}] GIT is NOT Installed on Your System! You can't use AutoUpdater .") 
                 sys.exit()
         else:
             try:
                 git_path = subprocess.check_output("which git", shell=True)
                 check_and_update()
             except Exception:
-                print(f"{WHITE}[{RED}ERR{WHITE}] GIT is NOT Installed on Your System! You can't use AutoUpdater .")              
+                print(f"{Fore.WHITE}[{Fore.RED}ERR{Fore.WHITE}] GIT is NOT Installed on Your System! You can't use AutoUpdater .")              
                 sys.exit()
     try:
         if arguments.show_fingerprint:
@@ -239,30 +244,34 @@ if __name__ == '__main__':
                 number += 1
         
         elif arguments.subdomain_list:
-            print("==================================================================")
-            print(f"[*] Adding Appropriate Web Protocal to Subdomains using httpx ...")
-            if "\\" in arguments.subdomain_list:
-                filename = arguments.subdomain_list.split("\\")[-1]
-                
-            elif "\\\\" in arguments.subdomain_list:
-                filename = arguments.subdomain_list.split("\\\\")[-1]
-                
-            elif "/" in arguments.subdomain_list:
-                filename = arguments.subdomain_list.split("/")[-1] 
-                
+            if arguments.skip_httpx_probing:
+                subdomain_list = readTargetFromFile(arguments.subdomain_list)
+
             else:
-                filename = arguments.subdomain_list
+                print("==================================================================") 
+                print(f"[*] Adding Appropriate Web Protocal to Subdomains using httpx ...")
+                if "\\" in arguments.subdomain_list:
+                    filename = arguments.subdomain_list.split("\\")[-1]
+                    
+                elif "\\\\" in arguments.subdomain_list:
+                    filename = arguments.subdomain_list.split("\\\\")[-1]
+                    
+                elif "/" in arguments.subdomain_list:
+                    filename = arguments.subdomain_list.split("/")[-1] 
+                    
+                else:
+                    filename = arguments.subdomain_list
+                    
+                outputFileName = arguments.subdomain_list.replace(filename, filename.replace(" ", "_")) + "_httpx.txt"    
                 
-            outputFileName = arguments.subdomain_list.replace(filename, filename.replace(" ", "_")) + "_httpx.txt"    
+                if AttackerSystem == "Windows":
+                    subprocess.run(f"type \"{arguments.subdomain_list}\" | \"{httpx_path}\" -threads 100 -o \"" + outputFileName + "\"", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                else:
+                    subprocess.run(f"cat \"{arguments.subdomain_list}\" | httpx -threads 100 -o \"" + outputFileName + "\"", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
             
-            if AttackerSystem == "Windows":
-                subprocess.run(f"type \"{arguments.subdomain_list}\" | \"{httpx_path}\" -threads 100 -o \"" + outputFileName + "\"", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-            else:
-                subprocess.run(f"cat \"{arguments.subdomain_list}\" | httpx -threads 100 -o \"" + outputFileName + "\"", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-           
-            print(f"[+] Done !")
-            print("==================================================================\n")        
-            subdomain_list = readTargetFromFile(outputFileName)
+                print(f"[+] Done !")
+                print("==================================================================\n")        
+                subdomain_list = readTargetFromFile(outputFileName)
             
             final_subdomain_list = split_list(subdomain_list, int(arguments.thread))
 
